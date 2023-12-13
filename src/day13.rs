e374aa240d86ba7323a6bf76eab8d7ef887b63bd
Adam Sunderland <iterion@gmail.com>
2023-12-13 11:37:36 -0500

    day 13 done

--- src/day13.rs
+++ src/day13.rs
@@ -4,11 +4,11 @@ pub(crate) fn run() -> (usize, usize) {
 }
 
 fn answer_part_1(lines: &str) -> usize {
-    AllMaps::parse(lines).sum_values()
+    AllMaps::parse(lines).sum_values(false)
 }
 
 fn answer_part_2(lines: &str) -> usize {
-    lines.len()
+    AllMaps::parse(lines).sum_values(true)
 }
 
 struct Map {
@@ -29,10 +29,10 @@ impl Map {
         Self { rows, cols }
     }
 
-    fn find_value(&self) -> usize {
-        if let Some(value) = self.horizontal_mirror_value() {
+    fn find_value(&self, v2: bool) -> usize {
+        if let Some(value) = self.horizontal_mirror_value(v2) {
             value
-        } else if let Some(value) = self.vertical_mirror_value() {
+        } else if let Some(value) = self.vertical_mirror_value(v2) {
             value
         } else {
             println!("Couldn't horizontal or vertical mirror!");
@@ -43,56 +43,115 @@ impl Map {
         }
     }
 
-    fn horizontal_mirror_value(&self) -> Option<usize> {
+    fn horizontal_mirror_value(&self, v2: bool) -> Option<usize> {
         let row_count = self.rows.len();
         for row in 0..row_count - 1 {
-            if self.rows[row] == self.rows[row + 1] {
-                if self.is_valid_horizontal_mirror(row) {
+            let a = &self.rows[row];
+            let b = &self.rows[row + 1];
+            if a == b {
+                if self.is_valid_horizontal_mirror(row, v2, false) {
                     return Some((row + 1) * 100);
+                }
+            } else if v2 {
+                let distance = levenshtein::levenshtein(a, b);
+                if distance <= 1 {
+                    if self.is_valid_horizontal_mirror(row, v2, distance == 1) {
+                        return Some((row + 1) * 100);
+                    }
                 }
             }
         }
         None
     }
 
-    fn is_valid_horizontal_mirror(&self, start: usize) -> bool {
+    fn is_valid_horizontal_mirror(&self, start: usize, v2: bool, was_smudged: bool) -> bool {
+        let mut was_smudged = was_smudged;
         let mut a = start as isize - 1;
         let mut b = start + 2;
         while a >= 0 && b < self.rows.len() {
-            if self.rows[a as usize] != self.rows[b] {
+            let row_a = &self.rows[a as usize];
+            let row_b = &self.rows[b];
+            if !v2 && row_a != row_b {
                 return false;
+            } else if v2 {
+                let distance = levenshtein::levenshtein(row_a, row_b);
+                // not close enough, always reject
+                if distance > 1 {
+                    return false;
+                } else if distance == 1 {
+                    // one smudge ok, two smudge get outta here
+                    // wasn't true in practice, but oh well
+                    if was_smudged {
+                        return false;
+                    }
+                    was_smudged = true;
+                }
             }
             a -= 1;
             b += 1;
         }
 
-        true
+        if v2 {
+            // if it wasn't smudged, we don't want it
+            was_smudged
+        } else {
+            true
+        }
     }
 
-    fn vertical_mirror_value(&self) -> Option<usize> {
+    fn vertical_mirror_value(&self, v2: bool) -> Option<usize> {
         let col_count = self.cols.len();
         for col in 0..col_count - 1 {
-            if self.cols[col] == self.cols[col + 1] {
-                if self.is_valid_vertical_mirror(col) {
-                    return Some(col + 1);
+            let a = &self.cols[col];
+            let b = &self.cols[col + 1];
+            if a == b {
+                if self.is_valid_vertical_mirror(col, v2, false) {
+                    return Some((col + 1));
+                }
+            } else if v2 {
+                let distance = levenshtein::levenshtein(a, b);
+                if distance == 1 {
+                    if self.is_valid_vertical_mirror(col, v2, true) {
+                        return Some(col + 1);
+                    }
                 }
             }
         }
         None
     }
 
-    fn is_valid_vertical_mirror(&self, start: usize) -> bool {
+    fn is_valid_vertical_mirror(&self, start: usize, v2: bool, was_smudged: bool) -> bool {
+        let mut was_smudged = was_smudged;
         let mut a = start as isize - 1;
         let mut b = start + 2;
         while a >= 0 && b < self.cols.len() {
-            if self.cols[a as usize] != self.cols[b] {
+            let col_a = &self.cols[a as usize];
+            let col_b = &self.cols[b];
+            if !v2 && col_a != col_b {
                 return false;
+            } else if v2 {
+                let distance = levenshtein::levenshtein(col_a, col_b);
+                // not close enough, always reject
+                if distance > 1 {
+                    return false;
+                } else if distance == 1 {
+                    // one smudge ok, two smudge get outta here
+                    if was_smudged {
+                        return false;
+                    }
+                    was_smudged = true;
+                }
             }
             a -= 1;
             b += 1;
         }
 
-        true
+        if v2 {
+            // if it wasn't smudged, we don't want it
+            was_smudged
+        } else {
+            true
+        }
     }
 }
 
@@ -119,8 +178,8 @@ impl AllMaps {
         Self { maps }
     }
 
-    fn sum_values(&self) -> usize {
-        self.maps.iter().map(|m| m.find_value()).sum()
+    fn sum_values(&self, v2: bool) -> usize {
+        self.maps.iter().map(|m| m.find_value(v2)).sum()
     }
 }
 
@@ -135,7 +194,7 @@ mod tests {
     fn test_all_lines() {
         let lines = get_input_string();
 
-        assert_eq!(answer_part_1(lines), 0);
+        assert_eq!(answer_part_1(lines), 27202);
         assert_eq!(answer_part_2(lines), 0);
     }
 
@@ -158,5 +217,6 @@ mod tests {
     #[test]
     fn test_point_of_incidence() {
         assert_eq!(answer_part_1(SAMPLE_INPUT), 405);
+        assert_eq!(answer_part_2(SAMPLE_INPUT), 400);
     }
 }
