ff524d031ff68667b42f7be30f56a099e6d65b0b
Adam Sunderland <iterion@gmail.com>
2023-12-15 13:57:32 -0500

    Finish day 15

--- src/day15.rs
+++ src/day15.rs
@@ -8,11 +8,89 @@ fn answer_part_1(lines: &str) -> usize {
 }
 
 fn answer_part_2(lines: &str) -> usize {
-    lines.len()
+    let ops: Vec<_> = lines
+        .replace('\n', "")
+        .split(',')
+        .map(LabelOperation::parse)
+        .collect();
+
+    let mut storage = Storage {
+        boxes: std::collections::HashMap::new(),
+    };
+    println!("{ops:?}");
+    for op in ops {
+        storage.run_operation(&op);
+    }
+
+    let mut sum = 0;
+    for i in 0..256 {
+        if let Some(lenses) = storage.boxes.get(&i) {
+            for (j, lens) in lenses.iter().enumerate() {
+                sum += lens.value * (j + 1) * (i + 1)
+            }
+        }
+    }
+    sum
 }
 
 fn get_input_string() -> &'static str {
     include_str!("../inputs/day15.txt")
+}
+
+struct Storage {
+    boxes: std::collections::HashMap<usize, Vec<LabelOperation>>,
+}
+
+impl Storage {
+    fn run_operation(&mut self, operation: &LabelOperation) {
+        let hash = hash_algorithm(&operation.label);
+        let contents = self.boxes.entry(hash).or_default();
+        match operation.operation {
+            Operation::Remove => contents.retain(|l| operation.label != l.label),
+            Operation::Set => {
+                for op in contents.iter_mut() {
+                    if op.label == operation.label {
+                        op.value = operation.value;
+                        return;
+                    }
+                }
+                contents.push(operation.clone());
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Operation {
+    Remove,
+    Set,
+}
+
+#[derive(Debug, Clone)]
+struct LabelOperation {
+    label: String,
+    operation: Operation,
+    value: usize,
+}
+
+impl LabelOperation {
+    fn parse(op: &str) -> Self {
+        if op.ends_with('-') {
+            let label = &op[0..(op.len() - 1)];
+            Self {
+                label: label.to_owned(),
+                operation: Operation::Remove,
+                value: 0,
+            }
+        } else {
+            let (label, value) = op.split_once('=').unwrap();
+            Self {
+                label: label.to_owned(),
+                operation: Operation::Set,
+                value: value.parse().unwrap(),
+            }
+        }
+    }
 }
 
 fn hash_algorithm(input: &str) -> usize {
@@ -34,7 +112,7 @@ mod tests {
         let lines = get_input_string();
 
         assert_eq!(answer_part_1(lines), 517965);
-        assert_eq!(answer_part_2(lines), 0);
+        assert_eq!(answer_part_2(lines), 267372);
     }
 
     const SAMPLE_INITIALIZATION_SEQUENCE: &str =
@@ -63,10 +141,7 @@ mod tests {
 
     #[test]
     fn test_hash_sum() {
-        let sum: usize = SAMPLE_INITIALIZATION_SEQUENCE
-            .split(',')
-            .map(hash_algorithm)
-            .sum();
-        assert_eq!(sum, 1320);
+        assert_eq!(answer_part_1(SAMPLE_INITIALIZATION_SEQUENCE), 1320);
+        assert_eq!(answer_part_2(SAMPLE_INITIALIZATION_SEQUENCE), 145);
     }
 }
